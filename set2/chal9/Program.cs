cca5c3cf793391b54bc8f9657060d8799b6b8e2a
opcode <sith@zuluhotel.ca>
2020-05-13 00:27:15 -0400

    challenge 9 set 2 complete

---
+++ set2/chal9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using CryptoPals;
+
+namespace chal9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    byte[] input = Encoding.ASCII.GetBytes("YELLOW SUBMARINE");
+	    Console.WriteLine( "Input: " + BitConverter.ToString( input ) );
+            byte[] b = PKCS.Pad( input, 20 );
+
+	    Console.WriteLine( "Output: " + BitConverter.ToString( b ) );
+        }
+    }
+}
